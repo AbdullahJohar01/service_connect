91b150e2f8502f5098e0c042bc35ac8a963185d3
Abdullah Johar <your-email@example.com>
2026-08-12 21:51:47 +0500

    Add notification and review tests

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "notification is valid with required fields" do
+    notification = notifications(:one)
+
+    assert notification.valid?
+  end
+
+  test "notification requires notification type" do
+    notification = notifications(:one)
+    notification.notification_type = nil
+
+    assert_not notification.valid?
+    assert_includes notification.errors.full_messages, "Notification type can't be blank"
+  end
+
+  test "notification requires message" do
+    notification = notifications(:one)
+    notification.message = nil
+
+    assert_not notification.valid?
+    assert_includes notification.errors.full_messages, "Message can't be blank"
+  end
 end
